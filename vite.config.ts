08ddf85841dfc35aea3f291021311c4d7075fee2
LeoDumas <leo.ndumas980@gmail.com>
2024-06-16 17:39:09 +0200

    fix: manifest for icons

--- vite.config.ts
+++ vite.config.ts
@@ -15,25 +15,25 @@ export default defineConfig({
         theme_color: '#ffffff',
         icons: [
           {
-            "src": "/pwa-192x192.png",
+            "src": "/Yoka_pwa/pwa-192x192.png",
             "sizes": "192x192",
             "type": "image/png",
             "purpose": "any"
           },
           {
-            "src": "/pwa-512x512.png",
+            "src": "/Yoka_pwa/pwa-512x512.png",
             "sizes": "512x512",
             "type": "image/png",
             "purpose": "any"
           },
           {
-            "src": "/pwa-maskable-192x192.png",
+            "src": "/Yoka_pwa/pwa-maskable-192x192.png",
             "sizes": "192x192",
             "type": "image/png",
             "purpose": "maskable"
           },
           {
-            "src": "/pwa-maskable-512x512.png",
+            "src": "/Yoka_pwa/pwa-maskable-512x512.png",
             "sizes": "512x512",
             "type": "image/png",
             "purpose": "maskable"
